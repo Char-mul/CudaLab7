9a5573380fa728b153ce80c65312478ba1c4d0c6
char-mul <c.e.mulvey-2020@hull.ac.uk>
2023-03-21 16:35:46 +0000

    Ex 2 commit

--- CudaLab7/Drawing_cuda.cu
+++ CudaLab7/Drawing_cuda.cu
@@ -36,6 +36,12 @@
 
  // includes, cuda
 #include <helper_cuda.h>
+#include <iostream>
+#include "vec3.h"
+#include "sphere.h"
+#include "ray.h"
+#include "hitable_list.h"
+#include "hitable.h"
 
 typedef unsigned int uint;
 typedef unsigned char uchar;
@@ -43,27 +49,78 @@ typedef unsigned char uchar;
 
 cudaArray* d_imageArray = 0;
 
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ ) 
+void check_cuda(cudaError_t result, char const* const func, const char* const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+            file << ":" << line << " '" << func << "' \n";
+        // Make sure we call CUDA Device Reset before exiting 
+        cudaDeviceReset();
+        exit(99);
+    }
+}
 
-__global__ void d_render(uchar4* d_output, uint width, uint height) {
-    uint x = __umul24(blockIdx.x, blockDim.x) + threadIdx.x;
-    uint y = __umul24(blockIdx.y, blockDim.y) + threadIdx.y;
-    uint i = __umul24(y, width) + x;
+__device__ vec3 castRay(const ray& r, hitable** world) {
+    hit_record rec;
+    if ((*world)->hit(r, 0.0, FLT_MAX, rec)) {
+        return 0.5f * vec3(rec.normal.x() + 1.0f, rec.normal.y() + 1.0f, rec.normal.z() + 1.0f);
+    }
+    else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5f * (unit_direction.y() + 1.0f);
+        return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    }
+}
 
-    float u = x / (float)width;
+__global__ void create_world(hitable** d_list, hitable** d_world) {
+    if (threadIdx.x == 0 && blockIdx.x == 0) {
+        *(d_list) = new sphere(vec3(0, 0, -1), 0.5);
+        *(d_list + 1) = new sphere(vec3(0, -100.5, -1), 100);
+        *d_world = new hitable_list(d_list, 2);
+    }
+}
+
+
+__global__ void free_world(hitable** d_list, hitable** d_world) {
+    delete* (d_list);
+    delete* (d_list + 1);
+    delete* d_world;
+}
+
+
+__global__ void d_render(uchar4* d_output, uint width, uint height, hitable **d_world) {
+    uint x = blockIdx.x * blockDim.x + threadIdx.x;
+    uint y = blockIdx.y * blockDim.y + threadIdx.y;
+    uint i = y * width + x;
+
+    float u = x / (float)width;  //----> [0, 1]x[0, 1] 
     float v = y / (float)height;
 
-    u = 2.0 * u - 1.0;
+    u = 2.0 * u - 1.0; //---> [-1, 1]x[-1, 1] 
     v = -(2.0 * v - 1.0);
-
     u *= width / (float)height;
-    v *= height / (float)width;
 
-    float radius = 0.5;
+    u *= 2.0;
+    v *= 2.0;
 
-    if ((x < width) && (y < height)) {
-        if (u * u + v * v < (radius * radius)) {
-            d_output[i] = make_uchar4(0, 0, 0xff, 0);
-        }
+    vec3 eye = vec3(0, 0.5, 1.5);
+    float distFrEye2Img = 1.0;;
+    if ((x < width) && (y < height))
+    {
+        //for each pixel 
+        vec3 pixelPos = vec3(u, v, eye.z() - distFrEye2Img);
+        //fire a ray: 
+        ray r;
+        r.O = eye;
+
+        r.Dir = pixelPos - eye;    //view direction along negtive z-axis! 
+
+
+        vec3 col = castRay(r, d_world);
+        float red = col.x();
+        float green = col.y();
+        float blue = col.z();
+        d_output[i] = make_uchar4(red * 255, green * 255, blue * 255, 0);
     }
 }
 
@@ -74,13 +131,21 @@ extern "C" void freeTexture() {
 }
 
 // render image using CUDA
-extern "C" void render(int width, int height,  dim3 blockSize, dim3 gridSize,
-     uchar4 * output) {
+extern "C"
+void render(int width, int height, dim3 blockSize, dim3 gridSize, uchar4 * output)
+{
+
+    // make our world of hitables 
+    hitable** d_list;
+    checkCudaErrors(cudaMalloc((void**)&d_list, 2 * sizeof(hitable*)));
+    hitable** d_world;
+    checkCudaErrors(cudaMalloc((void**)&d_world, sizeof(hitable*)));
+    create_world << <1, 1 >> > (d_list, d_world);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
 
 
-            d_render << <gridSize, blockSize >> > (output, width, height);
-
-
+    d_render << <gridSize, blockSize >> > (output, width, height, d_world);
     getLastCudaError("kernel failed");
 }
 
---
+++ CudaLab7/ray.h
@@ -0,0 +1,20 @@
+#ifndef RAYH
+#define RAYH
+#include "vec3.h"
+
+class ray
+{
+    public:
+        ray() {}
+        ray(const vec3& a, const vec3& b) { O = a; Dir = b; }  
+        vec3 origin() const       { return O; }
+        vec3 direction() const    { return Dir; }
+        vec3 point_at_parameter(float t) const { return O + t*Dir; }
+
+        vec3 O;
+        vec3 Dir;
+};
+
+#endif 
+
+
